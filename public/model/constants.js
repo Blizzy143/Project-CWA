aab0296783c9b3bb41e52c3132b07b7ba327f23e
KRISHNA VAMSI NARLA <knarla@uco.edu>
2022-04-15 16:44:02 -0500

    commit 1

---
+++ public/model/constants.js
@@ -0,0 +1,13 @@
+export const DEV = true;
+
+export const COLLECTION_NAMES = {
+    PRODUCT: 'product',
+    PURCHASE_HISTORY: 'purchase_history',
+    ACCOUNT_INFO: 'account_info',
+    PRODUCT_COMMENTS:'product_comment',
+    PRODUCT_WISHLIST:'product_wishlist',
+}
+
+export const STORAGE_FOLDER_NAMES = {
+    PROFILE_PHOTOS: 'profile_photos/',
+}
